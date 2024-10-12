37dd8fe15ced410165a0a4e364728a4270867fbe
yuto <y.kanda839yuzyou@gmail.com>
2024-10-12 14:39:36 +0900

    タスク機能実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :board
     belongs_to :user
+    has_one_attached :eyecatch
 
     validates :title, presence: true
     validates :content, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,12 @@ class User < ApplicationRecord
   has_many :tasks,dependent: :destroy
   has_one :profile,dependent: :destroy
 
-  def has_written?(board)
+  def has_written_board?(board)
     boards.exists?(id: board.id)
+  end
+
+  def has_written_task?(task)
+    tasks.exists?(id: task.id)
   end
 
   def prepare_profile
