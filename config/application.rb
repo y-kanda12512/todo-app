f0ff33243b6dbebf27752459a701036f07ee2ded
yuto <y.kanda839yuzyou@gmail.com>
2024-10-13 12:56:06 +0900

    Gemfile編集

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'dotenv-rails' if defined?(Rails)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
